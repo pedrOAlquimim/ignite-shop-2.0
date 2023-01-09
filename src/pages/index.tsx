139269ea6ff9193fd58bbda66fa75519b75db1cc
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-01-09 17:13:59 -0300

    starting making product card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
+import Image from 'next/image'
+import camiseta1 from '../assets/shirts/1.png'
+import { Handbag } from 'phosphor-react'
+
 export default function Home() {
   return (
     <div>
+      <div>
+        <Image src={camiseta1} alt='' />
+        <div>
+          <div>
+            <h2>Camisa X</h2>
+            <span>R$ 79,90</span>
+          </div>
+
+          <button>
+            <Handbag size={24} weight='bold' />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
